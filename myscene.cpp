d2d66f7b2f4daf134aecfe18907e106e4f22800d
John Abraham <devnull@localhost>
2013-04-30 06:57:15 -0700

    Add a test button

--- myscene.cpp
+++ myscene.cpp
@@ -1,10 +1,20 @@
 #include "myscene.h"
-
+#include <QGraphicsProxyWidget>
 
 myscene::myscene()
 {
+    m_testButton = new QPushButton("Test");
+    connect(m_testButton, SIGNAL(clicked()), this, SLOT(testSlot()));
+    addWidget(m_testButton);
     QPixmap pix("/Users/john/ns-3/newcanvas1/ns3logo2.png","png");
     m_background = addPixmap(pix);
     m_background->setFlags(QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsSelectable);
+    m_background->setZValue(-100);
 
+}
+
+
+void myscene::testSlot()
+{
+    qDebug("Hi");
 }
--- myscene.h
+++ myscene.h
@@ -3,12 +3,17 @@
 
 #include <QGraphicsScene>
 #include <QGraphicsPixmapItem>
+#include <QPushButton>
 
 class myscene : public QGraphicsScene
 {
+Q_OBJECT
 public:
     myscene();
     QGraphicsPixmapItem * m_background;
+    QPushButton * m_testButton;
+public slots:
+    void testSlot();
 };
 
 #endif // MYSCENE_H
