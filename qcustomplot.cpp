57a6b019d373039ae870f99fc8c0b93d425813d3
John Abraham <devnull@localhost>
2014-01-29 06:35:11 -0800

    Update Copyright

--- qcustomplot.cpp
+++ qcustomplot.cpp
@@ -1,4 +1,7 @@
-/*
+/**************************************************************************
+**                                                                       **
+** QCustomPlot, an easy to use, modern plotting widget for Qt            **
+** Copyright (C) 2011, 2012, 2013 Emanuel Eichhammer                     **
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
