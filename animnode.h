7c610b5774eed2d8e794c9ade61a0cb2036c085e
Dmitrii Shakshin <d.shakshin@gmail.com>
2014-07-10 07:55:44 -0700

    Index: animnode.h
    ===================================================================

--- animnode.h
+++ animnode.h
@@ -15,6 +15,7 @@
  *
  * Author: John Abraham <john.abraham.in@gmail.com>
  * Contributions: Eugene Kalishenko <ydginster@gmail.com> (Open Source and Linux Laboratory http://dev.osll.ru/)
+ * 		  Dmitrii Shakshin <d.shakshin@gmail.com> (Open Source and Linux Laboratory http://dev.osll.ru/)
  */
 #ifndef ANIMNODE_H
 #define ANIMNODE_H
@@ -44,7 +45,7 @@ public:
     DOUBLE_COUNTER
   } CounterType_t;
 
-  AnimNode (uint32_t nodeId, qreal x, qreal y, QString nodeDescription);
+  AnimNode (uint32_t nodeId, uint32_t nodeSysId, qreal x, qreal y, QString nodeDescription);
   ~AnimNode ();
   void paint (QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
   void mouseMoveEvent (QGraphicsSceneMouseEvent *event);
@@ -58,6 +59,7 @@ public:
   bool getShowNodeTrajectory ();
   QColor getColor ();
   uint32_t getNodeId ();
+  uint32_t getNodeSysId ();
   qreal getWidth ();
   int getResourceId ();
   Ipv4Vector_t getIpv4Addresses ();
@@ -73,18 +75,23 @@ public:
   bool hasIpv4 (QString ip);
   bool hasMac (QString mac);
   void showNodeId (bool show);
+  void showNodeSysId (bool show);
+  bool isVisibleNodeSysId () const;
   void updateCounter (uint32_t counterId, qreal counterValue, CounterType_t counterType);
 
   qreal getDoubleCounterValue (uint32_t counterId, bool & result);
   uint32_t getUint32CounterValue (uint32_t counterId, bool & result);
   void updateBatteryCapacityImage (bool show);
+  void updateNodeSysId (uint32_t nodeSysId, bool show);
 
 private:
   QGraphicsTextItem * m_nodeDescription;
   uint32_t m_nodeId;
+  uint32_t m_nodeSysId;
   qreal m_x;
   qreal m_y;
   bool m_showNodeId;
+  bool m_showNodeSysId;
   Ipv4Vector_t m_ipv4Vector;
   MacVector_t m_macVector;
   int m_resourceId;
@@ -92,6 +99,7 @@ private:
   QPixmap m_batteryPixmap; //!< Battery image
   bool m_showBatteryCapcity;
 
+  QColor m_lastColor;
 
   CounterIdValueUint32_t m_counterIdToValuesUint32;
   CounterIdValueDouble_t m_counterIdToValuesDouble;
@@ -109,7 +117,7 @@ public:
 
   static AnimNodeMgr * getInstance ();
   AnimNode * getNode (uint32_t nodeId);
-  AnimNode * add (uint32_t nodeId, qreal x, qreal y, QString nodeDescription);
+  AnimNode * add (uint32_t nodeId, uint32_t nodeSysId, qreal x, qreal y, QString nodeDescription);
   uint32_t getCount ();
   QPointF getMinPoint ();
   QPointF getMaxPoint ();
@@ -118,6 +126,7 @@ public:
   void addMacAddress (uint32_t nodeId, QString mac);
   void setSize (qreal width, qreal height);
   void showNodeId (bool show);
+  void showNodeSysId (bool show);
   TimePosVector_t getPositions (uint32_t nodeId);
   void addAPosition (uint32_t nodeId, qreal t, QPointF pos);
   void showRemainingBatteryCapacity (bool show);
